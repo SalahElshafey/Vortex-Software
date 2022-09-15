d633119bfab4361a11d9a7443f970cc661d1d102
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2022-09-15 15:38:14 +0200

    Update Squad_BSD_SalahElshafey_SquadProject1.c

--- Squad_BSD_SalahElshafey_SquadProject1.c
+++ Squad_BSD_SalahElshafey_SquadProject1.c
@@ -1,50 +1,47 @@
 #include<stdio.h>
-//R=Right tringle /C=circle/s=square/P=pyramid
-
-void square(int height){
-
-   for(int i=0;i<height;i++){
-    for(int j=0;j<height;j++){
-        printf("*");
-    }
+#include<math.h>
+//r=Right tringle , c=circle , s=square ,p=pyramid
+void square(int height)
+{
+   for(int i=0;i<height;i++)
+   {
+    for(int j=0;j<height;j++) printf("*");
     printf("\n");
    }
 }
-void circle(int height){
-     float dist;
- 
+void circle(int height)
+{
+  float dist;
   // for horizontal movement
-  for (int i = 0; i <= height; i++) {
- 
+  for (int i = 0; i <= height; i++)
+  {
     // for vertical movement
     for (int j = 0; j <= height; j++) {
       dist = sqrt((i - height/2) * (i - height/2) +
                   (j - height/2) * (j - height/2));
- 
       // dist should be in the range (radius - 0.5)
       // and (radius + 0.5) to print stars(*)
       if (dist > (height/2) - 0.5 && dist < (height/2) + 0.5)
         printf("*");
       else
-       printf(" ");     
+       printf(" ");
     }
-
    printf("\n");
   }
 }
-
-void pyramid(int height){
+void pyramid(int height)
+{
     for(int i=1;i<=height;i++){
     for(int z=1;z<=(height-i);z++){
     printf(" ");}
     //to print *
     for(int symbol=1;symbol<=((2*i)-1);symbol++)
     printf("*");
-    
     printf("\n");
   }
 }
-void R_triangle(int height){
+void R_triangle(int height)
+{
 for(int i=0;i<height;i++){
     for(int j=0;j<=i;j++){
         printf("*");
@@ -52,33 +49,27 @@ for(int i=0;i<height;i++){
     printf("\n");
 }
 }
-
 typedef struct q{
 int height;
 char shape;
 }S;
-
-
-int main(){
+int main()
+{
 S Var;
-printf("for Right triangle:R\n for square:s\n for pyramid:P\n for Circle:C\n");
-printf("please enter Character: ");
+printf("To print the following shapes enter the equivalent character\nRight triangle : r\nSquare : s\nPyramid : p\nCircle : c\n");
+printf("Please enter shape character : ");
 scanf("%c",&Var.shape);
-printf("please enter height: ");
+printf("Please enter shape height : ");
 scanf("%d",&Var.height);
-if(Var.height<2){
-    printf("please enter a greater number");
-   return 0;}
-else{
-if(Var.shape=='s'){
-    square(Var.height);
-}else if(Var.shape=='R'){
-    R_triangle(Var.height);
-}else if(Var.shape=='P'){
-    pyramid(Var.height);
-}else{
-    circle(Var.height);
-}}
-
-
+while(Var.height < 2)
+{
+    printf("Please enter a number greater than 2 : ");
+    scanf("%d",&Var.height);
+}
+if(Var.shape=='s') square(Var.height);
+else if(Var.shape=='r') R_triangle(Var.height);
+else if(Var.shape=='p') pyramid(Var.height);
+else if (Var.shape=='c')circle(Var.height);
+else printf("Invalid charachter!");
+return 0;
 }
