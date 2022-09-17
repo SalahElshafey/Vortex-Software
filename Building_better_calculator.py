d41c236bd6740346a1bc45ff0df28fe10c1a024d
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2022-09-17 13:57:12 +0200

    Add files via upload

--- Building_better_calculator.py
+++ Building_better_calculator.py
@@ -1,3 +1,34 @@
+<<<<<<< HEAD
+from re import I
+
+
+x=float(input("please enter num 1: "))
+y=input("please enter operator: ")
+factorial=1
+if y=='!':
+    if int(x)<0:
+        print("NO FACTORIAL")
+    elif int(x)==0:
+        print("the factorial of 0 is 1")
+    else:
+     for i in range(1,int(x+1)):
+        factorial=factorial*i
+        i+=1
+     print("factorual is: ",factorial)
+else:
+ z=float(input("please enter num 2: "))
+
+
+ if y=='+':
+    print(x+z)
+ elif y=='-':
+    print(x-z)
+ elif y=='*':
+    print(x*z)
+ elif y=='/':
+        print(x/z)
+ else:
+=======
 x=float(input("please enter num 1: "))
 y=input("please enter operator: ")
 z=float(input("please enter num 2: "))
@@ -12,4 +43,5 @@ elif y=='*':
 elif y=='/':
         print(x/z)
 else:
+>>>>>>> d23591bd57ad0d327eb6090c960252343cf8ac97
     print("INvalid OPerator")
