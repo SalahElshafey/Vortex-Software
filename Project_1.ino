b03b1dbea09ffab355a12741973faf550a2a8420
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2022-09-28 21:14:45 +0200

    Add files via upload

---
+++ Project_1.ino
@@ -0,0 +1,43 @@
+#define sw 12
+#define Red 9
+#define yellow 10
+#define Green 11
+int c=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(sw,INPUT);
+pinMode(Red,OUTPUT);
+pinMode(yellow,OUTPUT);
+pinMode(Green,OUTPUT);
+digitalWrite(sw,HIGH);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(c==0){
+    digitalWrite(Red,HIGH);
+    digitalWrite(yellow,LOW);
+    digitalWrite(Green,LOW);
+    delay(1000);
+    
+  }if(c==1){
+    digitalWrite(yellow,HIGH);
+    digitalWrite(Red,LOW);
+    digitalWrite(Green,LOW);
+    delay(1000);
+   
+  }if(c==2){
+    digitalWrite(Green,HIGH);
+    digitalWrite(Red,LOW);
+    digitalWrite(yellow,LOW);
+    delay(1000);
+ 
+  }else{
+    digitalWrite(Red,LOW);
+     digitalWrite(yellow,LOW);
+     digitalWrite(Green,LOW);}
+  if(digitalRead(sw)==LOW){
+    c++;
+    if(c>2) c=0;
+    }
+  
+}
